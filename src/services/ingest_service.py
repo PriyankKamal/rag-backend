0b49635ffcf18b80a23d08df0f3e7f51258a80ae
PriyankKamal <priyankkamal8@gmail.com>
2026-04-21 18:19:27 +0530

    use mistral model

--- src/services/ingest_service.py
+++ src/services/ingest_service.py
@@ -1,33 +1,41 @@
 import os
 import shutil
+from dotenv import load_dotenv
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_mistralai import MistralAIEmbeddings
 from langchain_chroma import Chroma
+
+load_dotenv()
 
 
 def ingest_pdf(file):
-    os.makedirs("uploads",exist_ok=True)
+    os.makedirs("uploads", exist_ok=True)
 
     file_path = f"uploads/{file.filename}"
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    # Load PDF
     loader = PyPDFLoader(file_path)
     docs = loader.load()
 
+    # Split into chunks
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 50
+        chunk_size=500,
+        chunk_overlap=50
     )
 
     chunks = splitter.split_documents(docs)
 
-    embedding_model = HuggingFaceEmbeddings()
+    # Mistral Embeddings
+    embedding_model = MistralAIEmbeddings(model="mistral-embed")
 
+    # Store in Chroma
     vector_store = Chroma.from_documents(
-        documents = chunks,
+        documents=chunks,
         persist_directory="chroma",
         embedding=embedding_model
     )
--- src/services/rag_service.py
+++ src/services/rag_service.py
@@ -1,36 +1,40 @@
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
 
-embedding_model = HuggingFaceEmbeddings()
+# Mistral Embeddings
+embedding_model = MistralAIEmbeddings(model="mistral-embed")
 
-llm = ChatMistralAI(model="mistral-small-2506")
+# Chat Model
+llm = ChatMistralAI(model="mistral-small-latest")
 
-promt = ChatPromptTemplate(
-    [
-        ("system","Use only provided context. If answer missing say I could not find the answer in the context."),
-        ("human","Context:\n{context} \n\n Question:\n{question}")
-    ]
-)
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "Use only provided context. If answer missing say: I could not find the answer in the context."
+    ),
+    (
+        "human",
+        "Context:\n{context}\n\nQuestion:\n{question}"
+    )
+])
 
 
 def ask_rag(question):
-
     vector_store = Chroma(
-    persist_directory="chroma",
-    embedding_function=embedding_model
+        persist_directory="chroma",
+        embedding_function=embedding_model
     )
-    
+
     retriever = vector_store.as_retriever(
-    search_type="mmr",
-    search_kwargs={
-        "k":3,
-        "fetch_k":10,
-        "lambda_mult":0.5
+        search_type="mmr",
+        search_kwargs={
+            "k": 3,
+            "fetch_k": 10,
+            "lambda_mult": 0.5
         }
     )
 
@@ -38,11 +42,11 @@ def ask_rag(question):
 
     context = "\n\n".join(doc.page_content for doc in docs)
 
-    final_promt = promt.invoke({
-        "context":context,
-        "question":question
+    final_prompt = prompt.invoke({
+        "context": context,
+        "question": question
     })
 
-    response = llm.invoke(final_promt)
+    response = llm.invoke(final_prompt)
 
     return response.content
